32c6d0fb3250c4838b8d15bf8965dd0d1dd3e8da
HP100 <becxer87@gmail.com>
2016-01-08 02:21:00 +0900

    Update becxer.cpp

--- eating_puzzle/becxer.cpp
+++ eating_puzzle/becxer.cpp
@@ -1,26 +1,26 @@
 #include <iostream>                                                             
                                                                                 
-int ruby[22];                                                                   
-int map[22][100000];                                                            
+int food[22];                                                                   
+int foodsum[22][100000];                                                        
 using namespace std;                                                            
 int main(void){                                                                 
     int limit, n,sum;                                                           
     cin >> limit >> n;                                                          
     sum = 0;                                                                    
     for(int i = 0 ; i < n ; i++){                                               
-         cin >> ruby[i];                                                        
-         sum += ruby[i];                                                        
-         map[i][ruby[i]] += 1;                                                  
+         cin >> food[i];                                                        
+         sum += food[i];                                                        
+         foodsum[i][food[i]] += 1;                                              
          for(int r = 0; r <= limit; r++){                                       
-            if( i-1 >= 0 && map[i-1][r] >= 1){                                  
-                map[i][r] += map[i-1][r];                                       
-                map[i][r+ruby[i]] += map[i-1][r];                               
+            if( i-1 >= 0 && foodsum[i-1][r] >= 1){                              
+                foodsum[i][r] += foodsum[i-1][r];                               
+                foodsum[i][r+food[i]] += foodsum[i-1][r];                       
             }                                                                   
          }                                                                      
     }                                                                           
                                                                                 
     for(int k = limit; k >= 0 ; k--){                                           
-        if(map[n-1][k] > 0){                                                    
+        if(foodsum[n-1][k] > 0){                                                
             cout << k << endl;                                                  
             break;                                                              
         }                                                                       
